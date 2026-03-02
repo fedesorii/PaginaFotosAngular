0e5d0f86028b66111c5aa0e4f8e24a9760726d8c
Federico N Soricetti <fedenahusori@gmail.com>
2026-03-02 03:18:48 -0300

    Auto-generated commit

--- app/models/gallery.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Gallery {
-  id: string; // Ejemplo: 'boda-playa-2026'
-  title: string; // Ejemplo: 'Boda en la Playa'
-  coverImg: string; // URL de la imagen de portada
-  description: string;
-  images: string[]; // Lista de URLs de todas las fotos del evento
-}
