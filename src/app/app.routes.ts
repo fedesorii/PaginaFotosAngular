7d4a79da5e2d9f24d298031d1b35c76827656e64
Federico N Soricetti <fedenahusori@gmail.com>
2026-03-02 02:49:02 -0300

    v0.3 - Implementamos Cloudinary para cargar las imagenes y carpetas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { GalleryDetail } from './pages/gallery-detail/gallery-detail';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'about', component: About },
+  { path: 'about-us', component: About },
   { path: 'contact', component: Contact },
   { path: 'gallery/:id', component: GalleryDetail },
   { path: '**', redirectTo: '' }, // Redirige a Home si la ruta no existe
--- src/app/pages/gallery-detail/gallery-detail.ts
+++ src/app/pages/gallery-detail/gallery-detail.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, effect, inject, input, signal } from '@angular/core';
 import { Gallery } from '../../services/gallery';
 
 @Component({
@@ -13,4 +13,22 @@ export class GalleryDetail {
   id = input.required<string>(); // Recibe el ID de la galería desde la ruta
 
   info = computed(() => this.galleryService.getById(this.id())); // Obtiene la información de la galería usando el ID
+
+  // 2. Creamos un Signal vacío para guardar las fotos que llegarán de internet
+  photos = signal<string[]>([]);
+
+  constructor() {
+    // 3. El effect "escucha". Cuando detecta que entramos a una galería (hay info nueva)...
+    effect(() => {
+      const currentInfo = this.info();
+
+      if (currentInfo) {
+        // ...va a Cloudinary a buscar las fotos usando el Tag
+        this.galleryService.getPhotosByTag(currentInfo.tag).subscribe((urls) => {
+          // 4. Cuando llegan las URLs, las guardamos en el Signal "photos"
+          this.photos.set(urls);
+        });
+      }
+    });
+  }
 }
